829b6fc7a0016ab340c9b921583117dad65500e7
Daniel Lagos <lago0008@algonquinlive.com>
2019-04-01 23:49:08 -0400

    packages were added
    packages were added in the folder and index html structure was created
    
    sass is having issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
 const gulp = require('gulp')
 const sass = require ('gulp-sass')
+const browerSync = require('browser-sync').create()
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano=('cssnano')
 
 gulp.task('sass',function(){
-    return gulp
-    .src('scss/**/*.scss')
+const plugins =[
+    autoprefixer({browsers:['last 2 versions']}),
+    cssnano
+]
+return gulp.src([
+
+    'node_modules/jquery/dist/jquery.js',
+
+    'node_modules/bootstrap/dist/js/bootstrap.js',
+
+    'js/*.js'
+
+  ])    .src('scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('css'))
-    .pipe(postcss(plugins))
+    .pipe.postcss(plugins)
     .pipe(gulp.dest('./css/min'))
     .pipe(browerSync.stream())
-
 })
 gulp.task('default', function (){
     browerSync.init({server:'./'})
