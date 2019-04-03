8f5415d8728692487d774fd63b3e78d53a358e36
Daniel Lagos <lago0008@algonquinlive.com>
2019-04-03 14:36:06 -0400

    fix some bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 const gulp = require('gulp')
-const sass = require ('gulp-sass')
+const scss = require ('gulp-sass')
 const browerSync = require('browser-sync').create()
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
-const cssnano=('cssnano')
+const cssnano=require('cssnano')
 
-gulp.task('sass',function(){
+gulp.task('scss',function(){
 const plugins =[
     autoprefixer({browsers:['last 2 versions']}),
-    cssnano
+    cssnano()
 ]
 return gulp.src([
 
@@ -18,15 +18,16 @@ return gulp.src([
 
     'js/*.js'
 
-  ])    .src('scss/**/*.scss')
-    .pipe(sass())
+  ])    
+  .src('scss/**/*.scss')
+    .pipe(scss())
     .pipe(gulp.dest('css'))
-    .pipe.postcss(plugins)
+    .pipe(postcss(plugin)) 
     .pipe(gulp.dest('./css/min'))
     .pipe(browerSync.stream())
 })
 gulp.task('default', function (){
     browerSync.init({server:'./'})
-    gulp.watch('scss/**/*.scss', gulp.series('sass'))
+    gulp.watch('scss/**/*.scss', gulp.series('scss'))
     gulp.watch('*.html').on('change',browerSync.reload)
 })
